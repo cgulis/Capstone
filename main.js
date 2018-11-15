a48b22718a732908eebf1a6a57c65494b61a591d
Cristian Gulisano <cristian.gulisano@genesys.com>
2018-11-15 01:03:52 -0500

    Initial commit.

--- main.js
+++ main.js
@@ -1,50 +1,211 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  ipcMain
+} = require('electron')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+require('electron-reload')(__dirname);
+
 let mainWindow
+let addWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600
+  })
   mainWindow.loadFile('index.html')
-
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
+  mainWindow.webContents.openDevTools()
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+  Menu.setApplicationMenu(mainMenu)
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+ipcMain.on('openMapWindow', () => {
+  addWindow = new BrowserWindow({
+    width: 750,
+    height: 500,
+    title: 'Select Your Route'
+  })
+  addWindow.loadFile('map.html')
+  addWindow.webContents.openDevTools()
+})
+
+ipcMain.on('closeMapWindow', () => {
+  console.log("close window button selected");
+  addWindow.close();
+})
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 750,
+    height: 500,
+    title: 'Select Your Route'
+  })
+  addWindow.loadFile('map.html')
+}
+
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// Create menu template
+const mainMenuTemplate = [{
+  label: 'File',
+  submenu: [{
+      label: 'Use Map',
+      click() {
+        createAddWindow();
+      }
+    },
+    {
+      label: 'Import File'
+    },
+    {
+      label: 'Quit',
+      accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+      click() {
+        app.quit()
+      }
+    }
+  ]
+}]
+
+
+var tj = require('./node_modules/@mapbox/togeojson'),
+  fs = require('fs'),
+  DOMParser = require('xmldom').DOMParser;
+const haversine = require('haversine');
+var itemsProcessed = 0;
+var coordinateElev = new Array();
+
+const googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyC0qtIQto3cnhYg-SRWkZLB8tqi4M_RssA',
+  Promise: Promise
+});
+
+ipcMain.on('kml-file-name', (event, arg) => {
+  var kml = new DOMParser().parseFromString(fs.readFileSync(arg, 'utf8'));
+  var convertedWithStyles = tj.kml(kml, {
+    styles: true
+  });
+  var myJSON = JSON.stringify(convertedWithStyles);
+  var geoObj = JSON.parse(myJSON);
+
+  var coordinates = geoObj.features[1].geometry.coordinates;
+
+  getDistance(coordinates);
+  // event.sender.send('geo-json',coordinates);
+})
+
+function getDistance(coordArray) {
+  var coordDistance = new Array();
+  for (var i = 0; i < coordArray.length - 1; i++) {
+    var j = i + 1;
+    var start = {
+      latitude: coordArray[i][1],
+      longitude: coordArray[i][0]
+    }
+    var end = {
+      latitude: coordArray[j][1],
+      longitude: coordArray[j][0]
+    }
+    coordDistance.push([coordArray[i][1], coordArray[i][0], coordArray[i][2], Math.round(haversine(start, end, {
+      unit: 'meter'
+    }) * 100) / 100]);
+    if ((i + 2) == coordArray.length) {
+      coordDistance.push([coordArray[i + 1][1], coordArray[i + 1][0], coordArray[i + 1][2], 0]);
+    }
+  }
+  // console.log(coordDistance);
+  populateElevation(coordDistance)
+}
+
+function populateElevation(coordinates) {
+  for (var i = 0; i < coordinates.length; i++) {
+    getElevationAtPoint(coordinates[i], i, coordinates.length);
+  }
+}
+
+function getElevationAtPoint(coords, index, length) {
+  googleMapsClient.elevation({
+      locations: {
+        lat: coords[0],
+        lng: coords[1]
+      }
+    })
+    .asPromise()
+    .then(function (response) {
+      itemsProcessed++;
+      coordinateElev.push([index, coords[0], coords[1], Math.round(response.json.results[0].elevation * 100) / 100, coords[3], 0]);
+      if (itemsProcessed == length) {
+        coordinateElev.sort(function (a, b) {
+          return a[0] - b[0];
+        });
+        calcGradient(coordinateElev);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function calcGradient(array) {
+  for (var i = 0; i < array.length - 1; i++) {
+    var j = i + 1
+    array[i][5] = Math.atan((array[j][3] - array[i][3]) / array[i][4]) * (180 / Math.PI)
+  }
+  calcEnergyConsumption(array)
+  // mainWindow.webContents.send('filled-array',array)
+}
+
+function calcEnergyConsumption(pointsArr) {
+  var ur = 0.005
+  var alpha = 0.0
+  var m = 544
+  var g = 9.8
+  var nttw = 0.89
+  var rho = 1.225
+  var A = 2.1
+  var cd = .18
+  var v = 30.0
+  var Fte = 0.0
+  var de = 0.0
+  var ci = 0.0
+  var acc = 0.0
+
+  var energyConsumption = [];
+
+  for (var i = 0; i < pointsArr.length - 1; i++) {
+    de = pointsArr[i][4]
+
+    alpha = pointsArr[i][5]
+
+    Fte = (ur * m * g) + (0.5 * rho * A * cd * Math.pow(v, 2)) + (m * g * Math.sin(alpha)) + (ci * m * acc)
+    var power = Math.round(((Fte * de) / nttw) * 100) / 100;
+
+    energyConsumption.push({
+      elev: pointsArr[i][3],
+      dist: pointsArr[i][4],
+      pow: power,
+      alpha: alpha
+    })
+  }
+  mainWindow.webContents.send('filled-array', energyConsumption)
+}
--- renderer.js
+++ renderer.js
@@ -1,3 +1,117 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+
+var app = require('electron').remote;
+var dialog = app.dialog;
+// var fs = require('fs');
+var fs = require('fs');
+
+const {
+    ipcRenderer
+} = require('electron')
+
+document.getElementById('uploadButton').onclick = () => {
+    dialog.showOpenDialog((fileName) => {
+        if (fileName === undefined) {
+            alert("File not found.");
+            return;
+        }
+        ipcRenderer.send('kml-file-name', String(fileName));
+    });
+};
+
+document.getElementById('routeButton').onclick = () => {
+    ipcRenderer.send('openMapWindow');
+}
+
+ipcRenderer.on('geo-json', (event, arg) => {
+    console.log(arg);
+})
+
+ipcRenderer.on('filled-array', (event, arg) => {
+    var summ1 = 0;
+    var elevationDataPoints = [];
+    var powerDataPoints = [];
+
+    for (var i = 0; i < arg.length; i++) {
+        elevationDataPoints.push({
+            x: ((Math.round(summ1) * 100) / 100) / 1000,
+            y: arg[i].elev
+        })
+        summ1 += arg[i].dist;
+    }
+
+    var summ2 = 0;
+    for (var i = 0; i < arg.length; i++) {
+        powerDataPoints.push({
+            x: ((Math.round(summ2) * 100) / 100) / 1000,
+            y: arg[i].pow
+        })
+        summ2 += arg[i].dist;
+    }
+
+    var totalConsumption = 0;
+    for (var i = 0; i < arg.length; i++) {
+        if (arg[i].alpha > -0.00559) {
+            totalConsumption += arg[i].pow;
+        }
+    }
+
+    var ctx = document.getElementById('initChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'scatter',
+        data: {
+            datasets: [{
+                    label: 'elevation',
+                    yAxisID: 'elevation',
+                    fill: false,
+                    // backgroundColor: 'rgb(0, 0, 0)',
+                    borderColor: 'rgb(255, 156, 156)',
+                    data: elevationDataPoints,
+                    pointRadius: 0,
+                    lineTension: 0
+                },
+                {
+                    label: 'power',
+                    yAxisID: 'power',
+                    backgroundColor: 'rgb(66, 244, 229)',
+                    data: powerDataPoints,
+                    pointRadius: 1,
+                    lineTension: 0
+                }
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                        id: 'elevation',
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Elevation in m'
+                        },
+                        position: 'left'
+                    },
+                    {
+                        id: 'power',
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Power in J'
+                        },
+                        position: 'right'
+                    }
+                ],
+                xAxes: [{
+                    ticks: {
+                        min: 0
+                        // max: dataPoints[dataPoints.length-1][1]
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Distance in km'
+                    }
+                }]
+            }
+        }
+    });
+    document.getElementById("consumed-energy").innerHTML = "Estimated Energy Consumed Along Route: " + (Math.round(totalConsumption / 3600) * 100) / 100 + " Wh";
+})
